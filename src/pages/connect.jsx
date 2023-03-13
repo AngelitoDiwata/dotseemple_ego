899bddf3617d7426b58b71bbc2c8a82d836cb477
AngelitoDiwata <diwataangelito@gmail.com>
2023-03-13 23:03:54 +0800

    Lagging stable

--- src/pages/connect.jsx
+++ src/pages/connect.jsx
@@ -133,43 +133,44 @@ export default function Home() {
                     </div>
                 }
                 {
-                    loginState && <div>
-                        <div className="absolute z-30 bg-black top-0 w-full m-auto flex flex-col md:flex-row items-start md:items-end justify-between py-5 space-y-3 md:space-x-3 space-x-0 md:space-y-0 px-10">
-                            {/* <input placeholder="🔍 Search for a handle" className="w-full md:w-1/3 lg:w-96 self-start border border-white bg-black rounded-lg outline-white px-3 py-2" value={searchVal} onChange={(e) => changeHandler(e, setSearchVal)} /> */}
-                            {/* <button className="w-full md:w-20 hover:scale-110 transition-all font-semibold border hover:font-neutral-900 hover:border-2 border-white bg-black rounded-lg outline-white px-3 py-1 text-white" onClick={() => fetchDB(searchVal)}>Search</button> */}
-                            <div className='w-full md:w-2/3 lg:w-1/3 flex flex-col md:flex-row items-center justify-end space-x-0 space-y-3 md:space-x-2 md:space-y-0'>
-                                <span className='w-1/2 text-xs text-white'>Active Codes: <span className='text-xl font-black'>{validCodes.filter((item) => new Date(item.ttl) > new Date()).map((item) => item.code).length} </span> </span>
-                                <input placeholder="CODE" className="w-full md:w-2/3 border border-white bg-black rounded-lg outline-white px-3 py-2" value={code} onChange={(e) => changeHandler(e, setCode)} />
-                                <button className="w-full md:w-20 hover:scale-110 transition-all font-semibold border hover:font-neutral-900 hover:border-2 border-white bg-black rounded-lg outline-white px-3 py-1 text-white" onClick={validateEntry}>submit</button>
-
-                            </div>
+                    loginState && <div className="absolute z-30 bg-black top-0 w-full m-auto flex flex-col md:flex-row items-start md:items-end justify-between py-5 space-y-3 md:space-x-3 space-x-0 md:space-y-0 px-10">
+                        <div className='w-full md:w-fit flex flex-row items-center justify-start space-x-3'>
+                            <input placeholder="🔍 Search for a handle" className="w-1/2 md:w-1/3 lg:w-96 self-start border border-white bg-black rounded-lg outline-white px-3 py-2" value={searchVal} onChange={(e) => changeHandler(e, setSearchVal)} />
+                            <button className="w-1/2 md:w-full lg:w-20 hover:scale-110 transition-all font-semibold border hover:font-neutral-900 hover:border-2 border-white bg-black rounded-lg outline-white px-3 py-1 text-white" onClick={() => fetchDB(searchVal)}>Search</button>
                         </div>
-                        <div className='bg-black w-full px-2 md:px-20 mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-5 h-fit pb-32 pt-44 md:pt-16'>
-                            {
-                                proritizedUserList().map((data, ind) => {
-                                    return <a key={ind} href={`https://twitter.com/${data.handle.replaceAll('@', '')}`} className={`hover:scale-110 transition-all h-full w-full px-5 py-3 m-auto flex flex-col space-x-5 items-center justify-center text-white my-5 bg-black rounded-full`}>
-                                        <div className="w-full flex flex-row items-center justify-center space-x-2">
-                                            <span className={`no-underline decoration-auto text-white tracking-widest ${data.handle === handle ? 'font-black' : 'text-xs'}`}
-                                            >{data.handle}</span>
-                                            <span className='text-xl md:text-5xl font-black flex flex-row items-start justify-start tracking-tighter'>⦿<sup className='text-xs tracking-normal font-light'>{data.connections}</sup></span>
-                                        </div>
-                                        <span style={{
-                                            'overflowWrap': 'break-word',
-                                            'wordWrap': 'break-word',
-                                            'hyphens': 'auto'
-                                        }} className='text-3xl mx-auto h-16 overflow-auto break-all w-full leading-3 text-left mb-5 font-black tracking-widest'>{
-                                                (<div>
-                                                    {[...Array(data.connections).keys()].map(() => {
-                                                        return '.'
-                                                    }).join('')}
-                                                </div>)
-                                            }</span>
-                                    </a>
-                                })
-                            }
+                        <div className='w-full md:w-2/3 lg:w-1/3 flex flex-col md:flex-row items-center justify-end space-x-0 space-y-3 md:space-x-2 md:space-y-0'>
+                            <span className='w-1/2 text-xs text-white'>Active Codes: <span className='text-xl font-black'>{validCodes.filter((item) => new Date(item.ttl) > new Date()).map((item) => item.code).length} </span> </span>
+                            <input placeholder="CODE" className="w-full md:w-2/3 border border-white bg-black rounded-lg outline-white px-3 py-2" value={code} onChange={(e) => changeHandler(e, setCode)} />
+                            <button className="w-full md:w-20 hover:scale-110 transition-all font-semibold border hover:font-neutral-900 hover:border-2 border-white bg-black rounded-lg outline-white px-3 py-1 text-white" onClick={validateEntry}>submit</button>
+
                         </div>
                     </div>
-
+                }
+                {
+                    loginState && <div className='bg-black w-full px-2 md:px-20 mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-5 h-fit pb-32 pt-44 md:pt-16'>
+                        {
+                            proritizedUserList().map((data, ind) => {
+                                return <a key={ind} href={`https://twitter.com/${data.handle.replaceAll('@', '')}`} className={`hover:scale-110 transition-all h-full w-full px-5 py-3 m-auto flex flex-col space-x-5 items-center justify-center text-white my-5 bg-black rounded-full`}>
+                                    <div className="w-full flex flex-row items-center justify-center space-x-2">
+                                        <span className={`no-underline decoration-auto text-white tracking-widest ${data.handle === handle ? 'font-black' : 'text-xs'}`}
+                                        >{data.handle}</span>
+                                        <span className='text-xl md:text-5xl font-black flex flex-row items-start justify-start tracking-tighter'>⦿<sup className='text-xs tracking-normal font-light'>{data.connections}</sup></span>
+                                    </div>
+                                    <span style={{
+                                        'overflowWrap': 'break-word',
+                                        'wordWrap': 'break-word',
+                                        'hyphens': 'auto'
+                                    }} className='text-3xl mx-auto h-16 overflow-auto break-all w-full leading-3 text-left mb-5 font-black tracking-widest'>{
+                                            (<div>
+                                                {[...Array(data.connections).keys()].map(() => {
+                                                    return '.'
+                                                }).join('')}
+                                            </div>)
+                                        }</span>
+                                </a>
+                            })
+                        }
+                    </div>
                 }
                 {
                     loginState && <footer className="absolute bg-black bottom-0 w-full flex flex-row items-center justify-center space-x-10 py-5">
