1d3e0768ec3965a641f398f84e4f2f40b67d0dce
AngelitoDiwata <diwataangelito@gmail.com>
2023-03-14 02:20:23 +0800

    Further code splitting

--- src/pages/connect.jsx
+++ src/pages/connect.jsx
@@ -1,19 +1,18 @@
 import { React, useState, useEffect } from 'react'
-import { set, ref, onValue, update } from "firebase/database";
+import { ref, onValue } from "firebase/database";
 import { fileText, twitter } from 'react-icons-kit/icomoon/'
 import { Icon } from 'react-icons-kit'
 import { db } from '@/firebase'
 import swal from 'sweetalert';
 import Head from 'next/head';
 import HeadForm from '@/components/HeadForm';
+import DotGrid from '@/components/DotGrid';
 
 export default function Home() {
     const [list, setList] = useState([])
     const [handle, setHandle] = useState("")
-    const [code, setCode] = useState("")
     const [validCodes, setValidCodes] = useState([])
     const [loginState, setLoginState] = useState(false)
-    const [searchVal, setSearchVal] = useState("")
 
     useEffect(() => {
         fetchDB()
@@ -101,30 +100,7 @@ export default function Home() {
                     />
                 }
                 {
-                    loginState && <div className='bg-black w-full px-2 md:px-20 mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-5 h-fit pb-32 pt-44 md:pt-16'>
-                        {
-                            proritizedUserList().map((data, ind) => {
-                                return <a key={ind} href={`https://twitter.com/${data.handle.replaceAll('@', '')}`} className={`hover:scale-110 transition-all h-full w-full px-5 py-3 m-auto flex flex-col space-x-5 items-center justify-center text-white my-5 bg-black rounded-full`}>
-                                    <div className="w-full flex flex-row items-center justify-center space-x-2">
-                                        <span className={`no-underline decoration-auto text-white tracking-widest ${data.handle === handle ? 'font-black' : 'text-xs'}`}
-                                        >{data.handle}</span>
-                                        <span className='text-xl md:text-5xl font-black flex flex-row items-start justify-start tracking-tighter'>⦿<sup className='text-xs tracking-normal font-light'>{data.connections}</sup></span>
-                                    </div>
-                                    <span style={{
-                                        'overflowWrap': 'break-word',
-                                        'wordWrap': 'break-word',
-                                        'hyphens': 'auto'
-                                    }} className='text-3xl mx-auto h-16 overflow-auto break-all w-full leading-3 text-left mb-5 font-black tracking-widest'>{
-                                            (<div>
-                                                {[...Array(data.connections).keys()].map(() => {
-                                                    return '.'
-                                                }).join('')}
-                                            </div>)
-                                        }</span>
-                                </a>
-                            })
-                        }
-                    </div>
+                    loginState && <DotGrid proritizedUserList={proritizedUserList} handle={handle} />
                 }
                 {
                     loginState && <footer className="absolute bg-black bottom-0 w-full flex flex-row items-center justify-center space-x-10 py-5">
