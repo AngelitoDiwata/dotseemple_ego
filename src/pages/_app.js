9a0817c21b3a3dce9cb631e4fc77470b02cd6b13
AngelitoDiwata <diwataangelito@gmail.com>
2023-03-16 13:56:23 +0800

    Disable devtools

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,5 @@
-import { disableReactDevTools } from '@/disableDevTools'
 import '@/styles/globals.css'
-
 export default function App({ Component, pageProps }) {
-    process.env.NODE_ENV === "production" && disableReactDevTools()
     return <Component {...pageProps }
     />
 }
