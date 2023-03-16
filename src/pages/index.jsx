7f52c0987ceb35b2c074bf3adb91c7356040afd5
AngelitoDiwata <diwataangelito@gmail.com>
2023-03-16 18:10:27 +0800

    fixed styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { twitter } from 'react-icons-kit/icomoon/'
 import { Icon } from 'react-icons-kit'
+import Footer from '@/components/Footer/Footer'
 
 export default function index() {
     return (
-        <div className='h-screen w-full flex flex-row items-center justify-center bg-black'>
-            <div className="card hover:scale-110 transition-all w-96 bg-neutral-800 shadow-xl shadow-neutral-900">
+        <div className='h-screen w-full flex flex-col items-center justify-between bg-black'>
+            <div className="card transition-all w-11/12 md:w-80 mt-10 bg-neutral-800 shadow-xl shadow-neutral-900">
                 <figure><img className='w-full' src="/breakfast.jpg" alt="breakfast" /></figure>
                 <div className="card-body">
                     <span className="card-title text-white font-thin flex flex-col items-center justify-center tracking-widest leading-6 text-justify">
@@ -15,12 +16,7 @@ export default function index() {
                     <a className="text-center font-thin hover:scale-110 transition-all text-sm text-white hover:underline" href="https://bit.ly/scratch-paper" target="_blank">bit.ly/scratch-paper</a>
                 </div>
             </div>
-            <footer className="absolute bg-black bottom-0 w-full flex flex-row items-center justify-center space-x-10 py-5">
-                <a className='no-underline decoration-auto text-white text-xs sticky bottom-0'
-                    href="https://twitter.com/dotseemple" target="_blank">
-                    <Icon icon={twitter} />
-                </a>
-            </footer >
+           <Footer />
         </div>
     )
 }
