4f66111a72a428ce28549e459816df6299b84639
AngelitoDiwata <diwataangelito@gmail.com>
2023-03-16 14:23:11 +0800

    Disabled DEV TOOLS FOR REAL

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,15 @@
 import '@/styles/globals.css'
+
+if (typeof window !== "undefined") {
+
+    const devtools = window !== undefined && window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+    if (window && devtools.renderers && Object.keys(devtools.renderers).length) {
+
+        devtools.renderers = {};
+
+    }
+}
+
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps }
     />
