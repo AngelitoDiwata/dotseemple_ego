ce9c5e9ad93537c506037948a1ec42629dcd8ac4
AngelitoDiwata <diwataangelito@gmail.com>
2023-03-18 01:11:31 +0800

    dot image

--- src/pages/connect.jsx
+++ src/pages/connect.jsx
@@ -86,11 +86,9 @@ export default function connect() {
                             {/* <FeaturedTweet /> */}
                         </div>
                         {
-                            loaded === 0 && <><div unoptimized style={{backgroundSize: '100vw 100vh', backgroundPosition: 'center center', backgroundRepeat: 'no-repeat' ,backgroundImage: 'url(\'dotype.gif\')', filter: 'brightness(70%)'}} className="md:visible w-full absolute bg-black h-screen m-auto z-40">
+                            loaded === 0 && <><div className="w-full absolute bg-black h-screen m-auto z-40">
 
-                            </div><div className="visible md:hidden w-full absolute bg-black h-screen m-auto z-40">
-
-</div></>
+                            </div></>
                         }
                     </>
             }
