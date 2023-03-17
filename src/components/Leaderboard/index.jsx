4947b7c6756ef06acd11abc2a202e67e73273729
AngelitoDiwata <diwataangelito@gmail.com>
2023-03-17 23:48:57 +0800

    right left align leaderboard

--- src/components/Leaderboard/index.jsx
+++ src/components/Leaderboard/index.jsx
@@ -17,7 +17,7 @@ export default function LeaderBoard({ isLoaded }) {
         <div className='text-white py-5 flex flex-col items-center justify-center space-x-2 w-11/12 md:w-4/5 m-auto mt-10 h-fit overflow-auto border border-gray-600'>
             <h3 className='text-base tracking-widest font-normal py-5'>LEADERBOARD</h3>
             <div className='flex flex-row items-start justify-center space-x-5'>
-                <div className='flex flex-col space-y-3 items-end justify-center'>
+                <div className='flex flex-col space-y-3 items-start justify-center'>
                     {
                         top40.filter((_, index) => {
                             return index < 20
