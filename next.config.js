a80b4fb88cd77055f4b3e8312c563db0f2a2d99c
AngelitoDiwata <diwataangelito@gmail.com>
2023-03-23 18:19:12 +0800

    Added obfuscation again after solving problem

--- next.config.js
+++ next.config.js
@@ -11,44 +11,44 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    // webpack: (config, { buildId, dev }) => {
-    //     if (!dev) {
-    //         config.plugins.push(new JavaScriptObfuscatorPlugin({
-    //             compact: true,
-    //             controlFlowFlattening: true,
-    //             controlFlowFlatteningThreshold: 0.75,
-    //             deadCodeInjection: true,
-    //             deadCodeInjectionThreshold: 0.4,
-    //             debugProtection: false,
-    //             debugProtectionInterval: 0,
-    //             disableConsoleOutput: true,
-    //             identifierNamesGenerator: 'hexadecimal',
-    //             log: false,
-    //             numbersToExpressions: true,
-    //             renameGlobals: false,
-    //             selfDefending: true,
-    //             simplify: true,
-    //             splitStrings: true,
-    //             splitStringsChunkLength: 10,
-    //             stringArray: true,
-    //             stringArrayCallsTransform: true,
-    //             stringArrayCallsTransformThreshold: 0.75,
-    //             stringArrayEncoding: ['base64'],
-    //             stringArrayIndexShift: true,
-    //             stringArrayRotate: true,
-    //             stringArrayShuffle: true,
-    //             stringArrayWrappersCount: 2,
-    //             stringArrayWrappersChainedCalls: true,
-    //             stringArrayWrappersParametersMaxCount: 4,
-    //             stringArrayWrappersType: 'function',
-    //             stringArrayThreshold: 0.75,
-    //             transformObjectKeys: true,
-    //             unicodeEscapeSequence: false
-    //         }, ['bundles/**/**.js']))
-    //     }
+    webpack: (config, { buildId, dev }) => {
+        if (!dev) {
+            config.plugins.push(new JavaScriptObfuscatorPlugin({
+                compact: true,
+                controlFlowFlattening: true,
+                controlFlowFlatteningThreshold: 0.75,
+                deadCodeInjection: true,
+                deadCodeInjectionThreshold: 0.4,
+                debugProtection: false,
+                debugProtectionInterval: 0,
+                disableConsoleOutput: true,
+                identifierNamesGenerator: 'hexadecimal',
+                log: false,
+                numbersToExpressions: true,
+                renameGlobals: false,
+                selfDefending: true,
+                simplify: true,
+                splitStrings: true,
+                splitStringsChunkLength: 10,
+                stringArray: true,
+                stringArrayCallsTransform: true,
+                stringArrayCallsTransformThreshold: 0.75,
+                stringArrayEncoding: ['base64'],
+                stringArrayIndexShift: true,
+                stringArrayRotate: true,
+                stringArrayShuffle: true,
+                stringArrayWrappersCount: 2,
+                stringArrayWrappersChainedCalls: true,
+                stringArrayWrappersParametersMaxCount: 4,
+                stringArrayWrappersType: 'function',
+                stringArrayThreshold: 0.75,
+                transformObjectKeys: true,
+                unicodeEscapeSequence: false
+            }, ['bundles/**/**.js']))
+        }
 
-    //     return config
-    // },
+        return config
+    },
     async headers() {
         return [{
             // matching all API routes
