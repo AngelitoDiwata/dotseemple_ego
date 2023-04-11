360113bd54370dccc0682cfc71317c4db1aefad9
AngelitoDiwata <diwataangelito@gmail.com>
2023-04-11 13:15:40 +0800

    DEV GO LIVE

--- src/components/LinkSubmission/index.jsx
+++ src/components/LinkSubmission/index.jsx
@@ -56,7 +56,7 @@ export default function LinkSubmission({ eligible, user, onSubmit }) {
             <div className='text-white flex flex-col items-end justify-center space-y-5 w-full m-auto border border-dashed p-3 rounded-lg'>
                 <div className='flex flex-col items-center justify-center space-y-1 w-full'>
                     <div className='w-full flex flex-row items-center text-center justify-start space-x-3'>
-                        <span className='text-base text-center w-full'><ReactMarkdown>{title}</ReactMarkdown></span>
+                        <span className='text-base text-justify w-full'><ReactMarkdown>{title}</ReactMarkdown></span>
                     </div>
                     <input onChange={(e) => setLink(e.target.value)} type="text" placeholder="https://twitter/com" className="input text-base font-normal border-white focus:outline-none hover:outline-none bg-black w-full" />
                 </div>
